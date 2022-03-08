d4fc507de325e6b9488712220d9b323790d296ba
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-03-08 15:10:34 -0300

    Adiciona o 1º requisito

--- src/average.js
+++ src/average.js
@@ -12,8 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  //codigo aqui
+const average = (array) => {
+  let soma = 0;
+  
+  if (array.length === 0) return undefined;
+  
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') return undefined;
+    soma += array[index];
+  }
+
+  const media = soma / array.length;
+  return Math.round(media);
 };
 
 module.exports = average;
